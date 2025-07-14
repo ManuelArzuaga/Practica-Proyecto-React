7ebc0879e452f159f736d5901818cddef5eccd39
arzuagamanuel <71405710+ManuelArzuaga@users.noreply.github.com>
2025-07-14 16:44:54 -0300

    firebase2

--- src/Config/Firebase.js
+++ src/Config/Firebase.js
@@ -4,15 +4,7 @@ import {getFirestore} from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyA-07q_48wQkGBFZP4CQ5riPZy49ZNIkqk",
-  authDomain: "productos-utn-af544.firebaseapp.com",
-  projectId: "productos-utn-af544",
-  storageBucket: "productos-utn-af544.firebasestorage.app",
-  messagingSenderId: "15138872181",
-  appId: "1:15138872181:web:27e51f96c4b4fc3437d13c"
-};
+//agregar la configuracion de firebase
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
